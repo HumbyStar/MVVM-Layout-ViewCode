e15f77c7664675c73ddf10b9733f540343fa3d97
Humberto <betogrt300@gmail.com>
2023-01-21 10:38:58 -0300

    Initial Commit

---
+++ MVVM + teste layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM + teste layout
+//
+//  Created by Humberto Rodrigues on 21/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
